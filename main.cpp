97fbaa565e56cf5492c3497e912e21ef60c5ba4b
Yinuo Huang <66022611+SnowballSH@users.noreply.github.com>
2022-07-18 22:27:49 +0000

    Some more features

--- main.cpp
+++ main.cpp
@@ -2,13 +2,38 @@
 #define ll long long
 using namespace std;
 
-void setIO(string s) {
-  ios_base::sync_with_stdio(0); cin.tie(0); 
+#define old_usaco false
+#define dfs_enabled false
+
+#if old_usaco
+void setIO(string s)
+{
   freopen((s + ".in").c_str(), "r", stdin);
   freopen((s + ".out").c_str(), "w", stdout);
 }
+#endif
 
-int main() {
-  
+#if dfs_enabled
+const int __SIZE = 1e5 + 10;
+vector<int> adj_list[__SIZE];
+bool visited[__SIZE];
+
+void dfs(int node)
+{
+  visited[node] = true;
+  for (int u : adj_list[node])
+    if (!visited[u])
+      dfs(u);
+}
+#endif
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+#if old_usaco
+  setIO("name");
+#endif
+
   return 0;
 }
