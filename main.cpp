0f8b1d661a79b882218f8c7c0bd1b813ffc71a34
Yinuo Huang <66022611+SnowballSH@users.noreply.github.com>
2022-08-01 21:42:42 +0000

    more

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@ using namespace std;
 #define old_usaco false
 #define dfs_enabled false
 
+#define get_vec(T, n, name)   \
+  vector<T> name(n);          \
+  for (int i = 0; i < n; i++) \
+    cin >> name[i];
+
+#define all_of(name) name.begin(), name.end()
+
+#define println(val) cout << val << '\n'
+
 #if old_usaco
 void setIO(string s)
 {
@@ -27,12 +36,33 @@ void dfs(int node)
 }
 #endif
 
+template <typename T>
+T last_true(T lo, const T hi, const function<bool(T)> f)
+{
+  lo--;
+  for (T dif = hi - lo; dif > 0; dif >>= 1)
+    while (lo + dif <= hi && f(lo + dif))
+      lo += dif;
+  return lo;
+}
+
+template <typename T, typename M>
+M reduce_f(const T lo, const T hi, const function<T(M, T)> f)
+{
+  M ans = 0;
+  for (T i = lo; i <= hi; i++)
+  {
+    ans = f(ans, i);
+  }
+  return ans;
+}
+
 int main()
 {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 #if old_usaco
-  setIO("name");
+  setIO("haybales.in");
 #endif
 
   return 0;
